ff39cc5ba1ae78a90863dd5b48647d5236ce82f1
Owen Jow <owenjow@berkeley.edu>
2016-06-03 03:32:35 -0700

    Renamed folder for test images

--- transcribe_test.py
+++ transcribe_test.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
-"""Tests for transcribe."""
+"""
+Tests for transcribe.
+Usage: `./transcribe_test.py`
+"""
 
 import pytest
 import transcribe
 
-class TestDetectTest:
+class TestDetectText:
     def test_gif_image_returns_text(self):
         vision = transcribe.VisionApi()
 
-        texts = vision.detect_text(['test-images/random_strip.gif'])
+        texts = vision.detect_text(['testdata/random_strip.gif'])
         document = transcribe.extract(texts).lower()
         
         assert 'ridiculous' in document
@@ -19,7 +22,7 @@ class TestDetectTest:
     def test_big_image_returns_text(self):
         vision = transcribe.VisionApi()
         
-        texts = vision.detect_text(['test-images/peanuts_movie.jpeg'])
+        texts = vision.detect_text(['testdata/peanuts_movie.jpeg'])
         document = transcribe.extract(texts).lower()
         
         assert 'peanuts' in document
